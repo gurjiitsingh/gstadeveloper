dad5b3bb47104811ba4ac60c43bad84bf8768e4c
gurnit <gurjiitsingh@gmail.com>
2025-05-18 16:16:20 +0530

    first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SiteProvider } from "@/SiteContext/SiteProvider";
-import { BargerMenu } from "@/Components/Bargermenu/BargerMenu"
+import { BargerMenu } from "@/components/Bargermenu/BargerMenu"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
