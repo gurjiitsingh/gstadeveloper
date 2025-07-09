07539917648ac9353c1c758e74280d763f7750aa
gurnit <gurjiitsingh@gmail.com>
2025-07-09 08:12:53 +0530

    update

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default function SinglePostPage({ params }:any) {
   return (
     <article className="max-w-3xl mx-auto px-4 py-10">
       <h1
-        className="text-3xl font-bold mb-4"
+        className="text-2xl font-bold mb-4"
         dangerouslySetInnerHTML={{ __html: post.title.rendered }}
       />
       <p className="text-sm text-gray-500 mb-4">
